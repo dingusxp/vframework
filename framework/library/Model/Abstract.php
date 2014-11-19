35e50bb9727118424e4adbcf3636083f75b65c77
unknown <xupeng03@E59EE659B0B48CF.internal.baidu.com>
2014-11-19 22:51:32 +0800

    model adapt

--- framework/library/Model/Abstract.php
+++ framework/library/Model/Abstract.php
@@ -4,5 +4,22 @@
  * Model 基类
  */
 abstract class Model_Abstract {
-    
+
+    /**
+     * 方法映射
+     * 当 Model 方法较多时，通过配置映射来将代码分散到多个文件而不用修改调用点
+     * @var array
+     */
+    protected static $_methodMapping = array();
+
+    /**
+     * 魔术方法 映射
+     */
+    public function __call($method, $param) {
+
+		if (isset(self::$_methodMapping[$method])) {
+			return call_user_func_array(self::$_methodMapping[$method], $param);
+        }
+		trigger_error('call an unknown method: '.__CLASS__.'::'.$method, E_USER_ERROR);
+    }
 }

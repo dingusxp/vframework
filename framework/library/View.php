9828102cba59e98af50b1e6b89eab930fb4cafe4
unknown <xupeng03@E59EE659B0B48CF.internal.baidu.com>
2014-11-20 13:04:46 +0800

    view add renderLayout method

--- framework/library/View.php
+++ framework/library/View.php
@@ -14,6 +14,11 @@ class View extends Component {
      * render 目标： web response
      */
     const RENDER_WEB_RESPONSE = 1;
+    
+    /**
+     * layout 重置模块中的 css和js 资源
+     */
+    const LAYOUT_REBUILD_RESOURCE = 1;
 
     /**
      * 工厂
--- framework/library/View/Abstract.php
+++ framework/library/View/Abstract.php
@@ -3,16 +3,16 @@
  * View 视图
  */
 abstract class View_Abstract {
-
+    
     /**
      * 数据
-     * @var <type>
+     * @var array
      */
     protected $_data = array();
 
     /**
      * 配置参数
-     * @var <type>
+     * @var array
      */
     protected $_option ;
 
@@ -34,8 +34,8 @@ abstract class View_Abstract {
 
     /**
      * 视图数据
-     * @param <type> $key
-     * @param <type> $value
+     * @param string $key
+     * @param mixed $value
      */
    public function assign($key, $value = null) {
 
@@ -50,8 +50,8 @@ abstract class View_Abstract {
 
     /**
      * 获取编译之后的文件
-     * @param <type> $tpl
-     * @return <type>
+     * @param string $tpl
+     * @return string
      */
     protected function _getCompiledFile($tpl) {
 
@@ -60,9 +60,9 @@ abstract class View_Abstract {
 
     /**
      * 视图渲染
-     * @param <type> $tpl
-     * @param <type> $target
-     * @return <type>
+     * @param string $tpl
+     * @param string $target
+     * @return string
      */
     public function render($tpl, $target = View::RENDER_WEB_RESPONSE) {
 
@@ -84,5 +84,70 @@ abstract class View_Abstract {
         }
         
         return $content;
+    }
+    
+    /**
+     * 将模块解析后放置到 layout 渲染
+     * @param array/string $moduleTpls
+     * @param string $layoutTpl
+     * @param array $tplData
+     * @param int $option
+     * @param int $target
+     */
+    public function renderLayout($moduleTpls, $layoutTpl, $tplData = array(), $option = 0, $target = View::RENDER_WEB_RESPONSE ) {
+
+        if (!is_array($moduleTpls)) {
+            $moduleTpls = array('mainContent' => $moduleTpls);
+        }
+        
+        // 参数指定变量
+        if ($tplData) {
+            foreach ($tplData as $key => $value) {
+                $this->assign($key, $value);
+            }
+        }
+
+        $extraScript = $extraStyle = '';
+        foreach ($moduleTpls as $name => $tpl) {
+            $content = $this->render($tpl, View::RENDER_NONE);
+            
+            // 提取模块中的 css、js，放置到 layout 指定位置
+            if ($option & View::LAYOUT_REBUILD_RESOURCE) {
+                $matches = array();
+                if (preg_match_all('/\<link rel\=\"stylesheet\".*?\/\>\s*/i', $content, $matches)) {
+                    foreach ($matches[0] as $value) {
+                        $extraStyle .= $value;
+                        $content = str_replace($value, '', $content);
+                    }
+                }
+                if (preg_match_all('/\<style type\=\"text\/css\".*?\<\/style\>\s*/ims', $content, $matches)) {
+                    foreach ($matches[0] as $value) {
+                        $extraStyle .= $value;
+                        $content = str_replace($value, '', $content);
+                    }
+                }
+                if (preg_match_all('/\<script type\=\"text\/javascript\" src\=\".*?\<\/script\>\s*/i', $content, $matches)) {
+                    foreach ($matches[0] as $value) {
+                        $extraScript .= $value;
+                        $content = str_replace($value, '', $content);
+                    }
+                }
+                if (preg_match_all('/\<script type\=\"text\/javascript"\>.*?\<\/script>\s*/ims', $content, $matches)) {
+                    foreach ($matches[0] as $value) {
+                        $extraScript .= $value;
+                        $content = str_replace($value, '', $content);
+                    }
+                }
+
+                // TO-DO 合并去除多余的 script 和 style 标签
+                
+            }
+            
+            $this->assign($name, $content);
+        }
+        
+        $this->assign('extraStyle', $extraStyle);
+        $this->assign('extraScript', $extraScript);
+        return $this->render($layoutTpl, $target);
     }
 }
